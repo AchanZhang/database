2036f8fabe7ea9c97f029f33f4ae9810eda4ac2f
张誉川 <zhangyuchuan@evercreative.com.cn>
2020-06-11 16:13:25 +0800

    最终测试通过

--- achan-api/src/main/java/com/achanzhang/mydatabase/achandatabase/controller/StudentController.java
+++ achan-api/src/main/java/com/achanzhang/mydatabase/achandatabase/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.achanzhang.mydatabase.achandatabase.controller;
 
 import com.achanzhang.mydatabase.achandatabase.beans.entry.StudentPO;
 import com.achanzhang.mydatabase.achandatabase.dao.StudentDao;
-import com.achanzhang.mydatabase.achandatabase.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/6/10
  */
 @RestController
-@RequestMapping("/achan/findStudent")
+@RequestMapping("/student")
 public class StudentController {
-    @Autowired
-    private StudentDao studentDao;
+    private final StudentDao studentDao;
 
-    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public StudentController(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    @RequestMapping(value = "/findStudentById/{id}", method = RequestMethod.GET)
     public StudentPO findStudent(@PathVariable() Integer id) {
         System.out.println("id:" + id);
         return studentDao.selectByPrimaryKey(id);
